1805fa7269e047bacf97c5fabb74736d9383335b
Mr. Horace <horacenehemiah@yahoo.com>
2017-02-28 20:05:29 -0600

    Revert "Firebase Email Authentication added for Sign In"
    This reverts commit 75d59324fa36e4c26a83b1f682bc219fc0b57d8e.

--- iHoop/iHoop/LoginViewController.swift
+++ iHoop/iHoop/LoginViewController.swift
@@ -7,13 +7,9 @@
 //
 
 import UIKit
-import FirebaseAuth
-import Firebase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var passwordText: UITextField!
-    @IBOutlet var emailText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,31 +21,15 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func signIn(_ sender: Any) {
-        if let email = emailText.text, let pwd = passwordText.text
-        {
-            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user,error) in
-                if error == nil {
-                    print("User Authenticated successfully")
-                }else {
-                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user,error) in
-                        if error != nil {
-                            print("Unable to authenticate using Email with Firebase")
-                        }else {
-                            print("Successfully authenticated with Firebase")
-                        }
-                    })
-                }
-            })
-            
-        }
-        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
-
-
-
-
-
-
-
